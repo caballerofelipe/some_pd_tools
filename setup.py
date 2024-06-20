2c4d56477af834ab04c1d56df1e15fbd2e08596c
Felipe Caballero <11650766+caballerofelipe@users.noreply.github.com>
2024-06-19 21:27:26 -0500

    Changed version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='some_pd_tools',
-    version='0.1.0',
+    version='0.1.1',
     description='Some tools to be used with Pandas.',
     package_dir={'': 'packages'},
     packages=find_packages(where='packages'),
